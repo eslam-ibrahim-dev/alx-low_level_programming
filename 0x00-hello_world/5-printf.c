fee12f97a8a8d959de4238d609d4ff0c1bd46a54
eslam-ibrahim-dev <aslam.ibrahim2022@gmail.com>
2023-02-18 13:20:38 -0800

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#incule <stdio.h>
+/**
+ * main - A program that prints function using printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
