a2aec878b07105a83e3f49a91f32817c36f9403a
eslam-ibrahim-dev <aslam.ibrahim2022@gmail.com>
2023-02-26 13:31:57 -0800

    finished

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * isLwer: check of the letter is in lowercase 
+ * Return: 0 success
+ */
+int _islower(int n)
+{
+	if ( n >= 97 && n <= 122)
+	{
+		return (1);
+	}
+		return (0);
+}
+		
