3ca6f8786516bbe534f85b85dfd81fd2a4f9f0c4
eslam-ibrahim-dev <aslam.ibrahim2022@gmail.com>
2023-02-26 12:15:44 -0800

    dasdas

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,20 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
+ * main: write function that prints chars
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
